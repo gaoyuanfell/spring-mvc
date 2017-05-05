ccce5ab018fd44e0cf21fb40d963a993bc765dbd
moka <gaoyuanfell@sina.com>
2017-05-05 15:42:59 +0800

    1

--- src/moka/basic/task/TaskJob.java
+++ src/moka/basic/task/TaskJob.java
@@ -1,11 +1,19 @@
 package moka.basic.task;
 
+import org.springframework.beans.factory.annotation.Value;
+
 import java.time.LocalDateTime;
 
 /**
  * Created by moka on 2017/4/14 0014.
  */
 public class TaskJob {
+
+    @Value("#{propertyConfigurer['data_root_path']}")
+    protected String rootPath;
+
+    @Value("#{propertyConfigurer['data_root_path_tmp']}")
+    protected String rootPathTmp;
 
     public void execute() {
         System.out.printf("Task: %s, Current time: %s\n", 1, LocalDateTime.now());
